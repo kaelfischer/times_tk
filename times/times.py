7405ab4ac247762e4315a1ded589ce8a1dfe7d7a
kael fischer <kael@ubiota.com>
2020-09-01 13:12:21 -0600

    flex maximum number

--- times/times.py
+++ times/times.py
@@ -3,7 +3,7 @@
 import tkinter as tk
 from tkinter.font import Font
 
-from random import randint
+from random import randint, choice
 
 
 
@@ -14,11 +14,18 @@ BIG = Font(top, size=30)
 PIXEL = tk.PhotoImage(width=1, height=1)
 
 top.CORRECT = False
-
 top.correct_answer = None
 
+max_number = 11
+numbers = tuple(range(1, max_number + 1))
+
+number_pairs = []
+for i in numbers:
+    for j in numbers:
+        number_pairs.append((i, j))
+
+
 frame = tk.Frame(top, pady=25,padx=80)#width=800,height=600)
-#frame.grid_propagate(0)
 frame.pack(fill=tk.BOTH, expand=True)
 
 
@@ -27,21 +34,30 @@ class ScoreGrid (tk.Toplevel):
     def __init__(self):
         super().__init__(top,bg='#666')
         self.cells = []
-        for row in range(13):
+        for row in range(max_number + 1):
             self.cells.append([])
-            for column in range(13):
-                cell = tk.Label(self, width=20, height=20, bd=1, bg='red', text='',
-                                 image=PIXEL, compound='c')
+            for column in range(max_number + 1):
+                if (row > 0) & (column > 0):
+                    cell_bg = 'red'
+                    cell_text = ''
+                else:
+                    cell_bg = 'white'
+                    cell_text = ''
+                    if row == 0:
+                        cell_text = str(column)
+                    elif column == 0:
+                        cell_text = str(row)
+
+                cell = tk.Label(self, width=20, height=20, bd=1, bg=cell_bg, text=cell_text,
+                                image=PIXEL, compound='c')
                 cell.grid(row=row,column=column)
                 self.cells[-1].append(cell)
 
     def mark_cell(self,color):
         row,column = top.number1,top.number2
-        print( row, column)
         cell = self.cells[row][column]
         cell.configure(background=color)
         cell.grid(row=row,column=column)
-        print(cell.configure())
 
 
 def check_answer(event):
@@ -65,8 +81,8 @@ def check_answer(event):
 
 
 def reset_problem():
-    top.number1 = randint(1,12)
-    top.number2 = randint(1,12)
+    top.number1, top.number2 = choice(number_pairs)
+    number_pairs.remove((top.number1, top.number2))
     top.correct_answer = top.number1 * top.number2
     top.CORRECT = False
 
